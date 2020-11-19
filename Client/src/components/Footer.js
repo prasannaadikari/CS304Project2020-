4b98a5a9cc7d143140d91ab598e3554e94c8a738
prasannaadikar <s16306@sci.pdn.ac.lk>
2020-11-19 07:32:31 +0530

    Add Header and Footer

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="pt-5">
-      <div className="container text-center">
-        <p>&copy; Chatty 2020.</p>
-      </div>
-    </footer>
+    <footer className="pt-3 mt-5">
+        <div className="bg-dark pt-5">
+            <div className="p-3"></div>
+                <div className="container">
+                    <div className="row ">
+                        <div className="col-md-4 text-center text-md-left ">
+                            <div className="py-0">
+                                <p className="text-light py-4 mb-4">&copy;2020 All rights reserved</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4 text-center text-md-left ">
+                            <div className="py-0">
+                                <p className="footer-links font-weight-bold">
+                                    <span className="text-white font-weight-bold ">Address: No 41,peradeniya road,Kandy.</span>
+                                </p>
+                                    <span className="text-white font-weight-bold ">🤳Phone: 099 1234567</span>
+                            </div>
+                        </div>
+                        <div className="col-md-4 text-center text-md-left ">
+                            <div className="py-0">
+                                <p className="footer-links font-weight-bold">
+                                    <span className="text-white font-weight-bold ">About Us:</span>
+                                </p>
+                                    <p className="text-light" >Our Service Centre facilitates in all necessary services which is our priority at all costs.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>               
   )
 }
 

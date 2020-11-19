e661c754bc952f3098725643e1c3f679f807a21c
prasannaadikar <s16306@sci.pdn.ac.lk>
2020-11-19 07:39:40 +0530

    Add Header

--- Admin/src/components/Header.js
+++ Admin/src/components/Header.js
@@ -7,20 +7,18 @@ function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-sm fixed-top navbar-light bg-light">
-        <Link className="navbar-brand" to="/">AutoVehicles</Link>
+        <Link className="navbar-brand" to={ROUTES.HOME}>Home</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          {auth().currentUser
-            ? <div className="navbar-nav">
+          
+              <div className="navbar-nav">
               <Link className="nav-item nav-link mr-3" to={ROUTES.APPOINTMENT}>Appointments</Link>
-              <button className="btn btn-primary mr-3" onClick={() => auth().signOut()}>Logout</button>
+              <Link className="nav-item nav-link mr-3" to={ROUTES.SEARCH}>Search</Link>
             </div>
-            : <div className="navbar-nav">
-              <Link className="nav-item nav-link mr-3" to={ROUTES.LOG_IN}>Log in</Link>
-              <Link className="nav-item nav-link mr-3" to={ROUTES.SIGN_UP}>Sign Up</Link>
-            </div>}
+             <div className="navbar-nav">
+            </div>
         </div>
       </nav>
     </header>

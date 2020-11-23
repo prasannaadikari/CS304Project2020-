7f706f551cb38a245f1e91cb0c57a5921a1cfb6e
prasannaadikar <s16306@sci.pdn.ac.lk>
2020-11-23 06:18:30 +0530

    Add search appointment page

---
+++ Admin/src/Pages/SearchAppointment/Appointments.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import {Checkmark} from "react-checkmark";
+import Db from "../../helpers/Db";
+import {Card, CardTitle, Form,Input,Container, FormGroup, CardBody, Button, Row, Col} from 'reactstrap';
+import moment from 'moment'
+import Appointment from "./component";
+import * as ROUTES from '../../helpers/routes';
+import Header from "../../components/Header";
+
+function searchingFor(search){
+  return function(x){
+      return x.Adate.toLowerCase().includes(search.toLowerCase()) || !search;
+  }
+}
+
+export default class AppointmentsList extends Component {
+  constructor(props) {
+    super(props);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveAppointment = this.setActiveAppointment.bind(this);
+    this.onDataChange = this.onDataChange.bind(this);
+    this.searchHandler = this.searchHandler.bind(this);
+
+    this.state = {
+      appointments: [],
+      search:"",
+      currentAppointment: null,
+      currentIndex: -1,
+    };
+  }
+  searchHandler(event){
+    this.setState({ search:event.target.value})
+}
+
+  componentDidMount() {
+    Db.getAllAppointments().on("value", this.onDataChange);
+  }
+
+  componentWillUnmount() {
+    Db.getAllAppointments().off("value", this.onDataChange);
+  }
+
+  onDataChange(items) {
+    let appointments = [];
+
+    items.forEach((item) => {
+      let key = item.key;
+      let data = item.val();
+
+      
+      appointments.push({
+        key: key,
+        VNo: data.VNo,
+        Adate: data.Adate,
+        status: data.status,
+        description: data.description,
+        uid: data.uid
+      });
+    });
+
+    this.setState({
+      appointments: appointments,
+    });
+  }
+
+  refreshList() {
+    this.setState({
+      currentAppointment: null,
+      currentIndex: -1,
+    });
+  }
+
+  setActiveAppointment(appointment, index) {
+    this.setState({
+      currentAppointment: appointment,
+      currentIndex: index,
+    });
+  }
+
+ 
+
+  render() {
+    const { appointments,search, currentAppointment, currentIndex } = this.state;
+
+    return (
+      <div className="p-5"> <Header/><div className="p-5">
+      
+        <div>
+      <Row><Col md="4">
+          <h4>Appointments</h4>
+          <Form><FormGroup><Input type="text" name="search" id="search" placeholder="Enter appointment date" value={search} onChange={this.searchHandler}/></FormGroup></Form>
+    
+          <ul style={{ 'overflow-y': 'scroll', 'overflow-x': 'hidden' }}>
+                 {appointments.filter(searchingFor(search)).map((appointment, index) => (
+                  <li className="list-group" tag="a" href="#" action className={ "list-group-item " + (index === currentIndex ? "active" : "") } 
+                  onClick={() => this.setActiveAppointment(appointment, index)}
+                   key={index}>
+                  <Row>
+                  <Col>
+                  <b>{appointment.VNo}</b>
+                  </Col>
+                  <Col> 
+                        {appointment.description ? <Checkmark size='medium'/>:null} 
+                  </Col>
+                  <Col xs="auto">
+                  {appointment.status==="Waiting"?<div className="ml-3 text-left text-uppercase text-warning" ><b>{appointment.status}</b></div>:null}
+                  {appointment.status==="Processing"?<div className="ml-3 text-left text-uppercase text-primary" ><b>{appointment.status}</b></div>:null}
+                  {appointment.status==="Done"?<div className="ml-3 text-left text-uppercase text-success" ><b>{appointment.status}</b></div>:null}
+                  </Col>
+                  </Row>
+                </li>
+                ))}
+          </ul>
+      
+        </Col><Col md="8">
+            <div>
+              {currentAppointment ? (
+                <Appointment
+                  appointment={currentAppointment}
+                  refreshList={this.refreshList}
+                />
+              ) : (
+              <div>
+                <br />
+                  <p>Please click on a Appointment...</p>
+                </div>
+              )}
+            </div>
+            </Col></Row></div>
+      </div></div>
+    );
+  }
+}
---
+++ Admin/src/Pages/SearchAppointment/component.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import Db from "../../helpers/Db";
+import {Card, CardTitle, Form,Input,Container, FormGroup, Label, CardBody, Button, Row, Col} from 'reactstrap';
+
+
+export default class Appointment extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangestatus = this.onChangestatus.bind(this);
+    this.onChangedescription = this.onChangedescription.bind(this);
+    this.updateAppointment = this.updateAppointment.bind(this);
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    
+    this.state = {
+      currentAppointment: {
+        key: null,
+        VNo: "",
+        description: "",
+        uid:"",
+        status: false,
+      },
+      message: "",
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { appointment } = nextProps;
+    if (prevState.currentAppointment.key !== appointment.key) {
+      return {
+        currentAppointment: appointment,
+        message: ""
+      };
+    }
+
+    return prevState.currentAppointment;
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentAppointment: this.props.appointment,
+    });
+  }
+
+  onChangestatus(e) {
+    const status = e.target.value;
+
+    this.setState(function (prevState) {
+      return {
+        currentAppointment: {
+          ...prevState.currentAppointment,
+          status: status,
+        },
+      };
+    });
+  }
+
+  onChangedescription(e) {
+    const description = e.target.value;
+
+    this.setState((prevState) => ({
+      currentAppointment: {
+        ...prevState.currentAppointment,
+        description: description,
+      },
+    }));
+  }
+
+  updateAppointment() {
+    const data = {
+      status: this.state.currentAppointment.status,
+      description: this.state.currentAppointment.description,
+    };
+
+    Db.update(this.state.currentAppointment.key, data)
+      .then(() => {
+        this.setState({
+          message: "The Appointment was updated successfully!",
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  deleteAppointment() {
+    Db.delete(this.state.currentAppointment.key)
+      .then(() => {
+        this.props.refreshList();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+
+  render() {
+    const { currentAppointment} = this.state;
+
+    return (
+      <div className="p-5">
+  
+        <h4>Appointment</h4>
+        {currentAppointment
+         ? (
+          <Container>
+          <div className="py-3">
+            <Card  className="h-100 shadow" style={{ 'background': '#FFF', 'color': '#000' }}>
+              <CardBody>
+                  <Row>
+                    <Col xs="6">
+                      <CardTitle ><h3><b>{currentAppointment.VNo}</b></h3></CardTitle>
+                      <CardTitle >{currentAppointment.Adate}</CardTitle>
+                    </Col>
+                    <Col xs="auto"><Row>
+                      {currentAppointment.status==="Waiting"  ?<CardTitle className="ml-3 text-left text-uppercase text-warning" ><b>{currentAppointment.status}</b></CardTitle> :null}
+                      {currentAppointment.status==="Processing"  ?<CardTitle className="ml-3 text-left text-uppercase text-primary" ><b>{currentAppointment.status}</b></CardTitle> :null}
+                      {currentAppointment.status==="Done"  ?<CardTitle className="ml-3 text-left text-uppercase text-success" ><b>{currentAppointment.status}</b></CardTitle> :null}            
+                    </Row><Row>
+                      <CardTitle >Uid<b>  [</b>{currentAppointment.uid}<b>]</b></CardTitle>
+                    </Row>
+                    </Col>    
+                  </Row>
+                  <Form >
+                    <Row>
+                      <Col xs="auto">
+                        <FormGroup>
+                          <Label for="address">Update status</Label>
+                          <Input type="select" name="St" id="St" value={currentAppointment.status} onChange={this.onChangestatus}>
+                          <option className="d-none">{currentAppointment.status}</option>
+                                    <option>"Waiting"</option>
+                                    <option>Processing</option>
+                                    <option>Done</option>
+                          </Input>
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <FormGroup>
+                          <Label for="description">Description</Label>
+                          <Input type="text" name="text" id="text"  placeholder="Vehicle service description" value={currentAppointment.description} onChange={this.onChangedescription}/>
+                        </FormGroup>
+                      </Col>  
+                    </Row>
+                    <Row>
+                    <Col sm="auto">
+                      <Button onClick={this.updateAppointment} className="btn-block" color="primary">Update</Button>
+                    </Col>
+                    <Col sm="auto">
+                      <Button onClick={this.deleteAppointment} className="btn-block" color="danger">Delete</Button>
+                    </Col>
+                    </Row>
+                  </Form>
+                </CardBody>
+            </Card>
+            </div>
+            
+
+          </Container>) 
+          : (
+          <div>
+            <br />
+            <p>Please click on a Appointment...</p>
+          </div>)}
+      
+    </div>
+    );
+  }
+}

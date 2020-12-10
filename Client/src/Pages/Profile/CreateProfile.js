d10906bf1d5ed6a30902cd469f197527a05a3ac4
prasannaadikar <s16306@sci.pdn.ac.lk>
2020-12-10 06:12:38 +0530

    Edit validation

--- Client/src/Pages/Profile/CreateProfile.js
+++ Client/src/Pages/Profile/CreateProfile.js
@@ -1,11 +1,10 @@
 
 import React, { Component } from 'react'
 import { Col, Row, Form, FormGroup, Label, Input, Button } from 'reactstrap'
-import { Link, withRouter } from 'react-router-dom'
 import { auth } from '../../services/firebase';
-import Header from "../../components/Header";
 import * as ROUTES from '../../helpers/routes';
 import Db from "../../helpers/Db";
+import Header from "../../components/Header";
 
 export class Profile extends Component {
     constructor(props) {
@@ -23,7 +22,7 @@ export class Profile extends Component {
           email: null,
           address: null,
           phone: null,
-          user: auth().currentUser,
+          user:auth().currentUser,
           error:null,
           msg: null,
         };
@@ -55,9 +54,11 @@ export class Profile extends Component {
         });
       }
 
-      saveProfile() { const {title,lastname,email,address,phone,user} = this.state;
+    saveProfile() { const {title,lastname,email,address,phone,user} = this.state;
       
-      if(!lastname.match(/^[a-zA-Z]+$/)) {
+      if(lastname === null ) {
+        this.setState({error:null});
+      }else if(!lastname.match(/^[a-zA-Z]+$/)) {
           this.setState({error:'Last name can only contain letters'}); 
           return;
       }else if((lastname === null && title === null) || (lastname !== null && title !== null)) {
@@ -69,7 +70,7 @@ export class Profile extends Component {
 
       if(email === null){
         this.setState({error:null});
-      }else if(!email.match(/\S+@\S+/)) { //  /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\W[\w-]{0.66})\.([a-z]{2.6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2\.}))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      }else if(!email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) { 
           this.setState({error:'Invalid email'});
           return;
       }
@@ -77,7 +78,7 @@ export class Profile extends Component {
       if(phone === null){
         this.setState({error:null});
       }else if(!phone.match(/\d{10}/)) {
-          this.setState({error:'Phone number cannot contain letters or space'}); 
+          this.setState({error:'Phone number cannot contain letters or space and should be ten digits.'}); 
           return;
       }
 
@@ -87,36 +88,24 @@ export class Profile extends Component {
             email:email,
             address:address,
             phone:phone,
-            uid:user.uid,
+            uid:auth().currentUser.uid,
         };
         Db.createProfile(data)
-        this.setState({msg: 'Created new appointment successfully!' });
+        this.setState({msg: 'Created new profile successfully!' });
         this.props.history.push(ROUTES.HOME);
       
-  }
-  skipProfile(){ 
-    let data = {
-      title:'',
-      lastname:'',
-      email:'',
-      address:'',
-      phone:'',
-      uid:this.state.user.uid,
-  };
-  Db.createProfile(data)
-  this.setState({msg: 'Created new appointment successfully!' });
-  this.props.history.push(ROUTES.HOME);
   }
 
     render() {  const {error,msg } = this.state;
         
         return ( 
+          <div><Header/>
             <div className="container col-lg-5 my-3 min-vh-100 d-flex flex-column justify-content-center">
               
                 <Form className="p-5" >
                     <h3 className="text-center">Account Settings</h3>
                     <h6 className="lead text-primary" >Fill in the form below to create your profile.</h6>
-                    <Button onClick={this.skipProfile} className="btn" color="info">SKIP</Button>
+                    <Button href={ROUTES.HOME} className="btn" color="info">SKIP</Button>
                     <hr className="mb-5" />
 
                     <Row>
@@ -161,7 +150,7 @@ export class Profile extends Component {
                     </FormGroup>
                 </Form>
 
-                
+                </div>
             </div>
         )
     }

b12091a699086db003e2de057bbab51284c2d8b8
prasannaadikar <s16306@sci.pdn.ac.lk>
2020-12-10 06:15:50 +0530

    Change layout

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="pt-3 mt-5">
-        <div className="bg-dark pt-5">
+        <div className="bg-dark pt-5 py-5">
             <div className="p-3"></div>
                 <div className="container">
                     <div className="row ">
@@ -14,19 +14,15 @@ function Footer() {
                         </div>
                         <div className="col-md-4 text-center text-md-left ">
                             <div className="py-0">
-                                <p className="footer-links font-weight-bold">
-                                    <span className="text-white font-weight-bold ">Address: No 41,peradeniya road,Kandy.</span>
+                                <p>
+                                    <span className="row col text-white font-weight-bold ">Address: No 41,peradeniya road,Kandy.</span>
                                 </p>
-                                    <span className="text-white font-weight-bold ">Phone: 099 1234567</span>
+                                    <span className="row col text-white font-weight-bold">Phone: 099 1234567</span>
+                                    <span className="row col text-white font-weight-bold">Email: AUTOservice@gmail.com</span>
                             </div>
                         </div>
-                        <div className="col-md-4 text-center text-md-left ">
-                            <div className="py-0">
-                                <p className="footer-links font-weight-bold">
-                                    <span className="text-white font-weight-bold ">About Us:</span>
-                                </p>
-                                    <p className="text-light" >Our Service Centre facilitates in all necessary services which is our priority at all costs.</p>
-                            </div>
+                        <div className="col-md-4">
+                            <span className="row col text-light" ><span className="row col text-white font-weight-bold ">About Us: </span>Our Service Centre facilitates in all necessary services which is our priority at all costs.</span>
                         </div>
                     </div>
                 </div>
